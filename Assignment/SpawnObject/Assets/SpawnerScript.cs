8910c023c06fab8750fd85767f86a18ed101b5f7
Groza-Frontline <151484854+Groza-Frontline@users.noreply.github.com>
2026-05-29 00:37:38 +0800

    Created Spawn Function with Sphere in game

---
+++ Assignment/SpawnObject/Assets/SpawnerScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject spherePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(spherePrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
