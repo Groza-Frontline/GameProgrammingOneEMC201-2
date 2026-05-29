d9622d5b5512b91b50fcc844614c91a2c78343c0
Groza-Frontline <151484854+Groza-Frontline@users.noreply.github.com>
2026-05-29 20:11:59 +0800

    Made a spawn project for Activity101

--- Assignment/SpawnObject/Assets/Scripts/SpawnerScript.cs
+++ Assignment/SpawnObject/Assets/Scripts/SpawnerScript.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class SpawnerScript : MonoBehaviour
 {
     private SpawnAction inputActions;
-    [SerializeField] private GameObject[] Prefabs; 
+    [SerializeField] private GameObject[] Prefabs;
+    [SerializeField] private Vector3[] randomPos;
+    [SerializeField] private Quaternion[] randomRot;
 
     private void Awake() => inputActions = new SpawnAction();
 
@@ -16,7 +18,17 @@ public class SpawnerScript : MonoBehaviour
     {
         inputActions.SpawnObj.Spawn.performed += ctx =>
         {
-            int randomPrefab = Random.Range(0, 3);
+            int randomIndex = Random.Range(0, Prefabs.Length);
+            int randomPosition = Random.Range(0, randomPos.Length);
+            int randomRotation = Random.Range(0, randomRot.Length);
+
+            GameObject newPrefab = Instantiate(Prefabs[randomIndex], randomPos[randomPosition], randomRot[randomRotation]);
+            newPrefab.AddComponent<Rigidbody>();
+            newPrefab.name = "CuriosCapsule";
+            newPrefab.name = "SmileySphere";
+            newPrefab.name = "ConfusedCube";
+            newPrefab.name = "ProudPlane";
+            newPrefab.name = "CryingCylinder";
         };  
     }
 
